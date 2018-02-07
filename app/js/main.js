a5471c14f43799a88ebd48d2b601ff039d424b45
Aziz Ramos <sonicthehamilton.ar@gmail.com>
2018-02-07 08:54:21 -0500

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,33 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+(function($) {
+	function pullStuff(keyword, target){
+
+		$.ajax({
+			type: "GET",
+			url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+keyword+"&callback=?",
+			contentType: "application/json; charset=utf-8",
+			async: false,
+			dataType: "json",
+			success: function (data, textStatus, jqXHR) {
+
+				var markup = data.parse.text["*"];
+
+				function strip(html){
+					var tmp = document.createElement("DIV");
+					tmp.innerHTML = html;
+					return tmp.textContent || tmp.innerText || "";
+				}
+
+				var cleanmarkup = strip(markup).replace(/(\r\n|\n|\r)/gm," ").replace(/\s\s+/g, ' ');
+
+				var blurb = $(target).append(cleanmarkup);			
+
+			},
+			error: function (errorMessage) {
+
+			}
+		});
+
+
+	}
+	pullStuff("Portal%3ACurrent_events","#target"); 
+})(jQuery);
