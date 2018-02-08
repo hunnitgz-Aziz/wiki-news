4b8cde9c73f43d15e0da3b1e697adadfd6082b30
Aziz Ramos <sonicthehamilton.ar@gmail.com>
2018-02-08 08:28:29 -0500

    removed marquee time to come up with another solution

--- app/js/lib/a-library.js
+++ app/js/lib/a-library.js
@@ -1,3 +1,20 @@
-(function() {
-  console.log('This is a library. It should be the first file');
+(function(){
+
+// function scroller() {
+
+//   var scroll = $('.scroll');// Sets the div with a class of scroll as a variable
+  
+//   var height = scroll.height(); // Gets the height of the scroll div
+  
+//   var topAdj = -height-50;  
+							 
+	
+// 	scroll.animate({
+// 		'top' : [topAdj, 'linear'] 
+// 	}, 1000, function(){
+// 		scroll.css('top', 80); //resets the top position of the Ul for the next cycle
+// 		scroller(); // Recalls the animation cycle to begin.
+// 	});}
+	
+// scroller();
 })();
